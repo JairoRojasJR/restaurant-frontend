348bf20b00695087a1d3ab513f424fa140075db4
Jairo Rojas <jairorojasofficial@gmail.com>
2023-10-12 18:01:38 -0500

    Gestión en base de datos y API de los platos del menú, conexión a azure storage y streaming de imágenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,83 @@
 'use client'
 
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import OrderPlate from '@/components/OrderPlate'
+import CardPlate from '@/components/CardPlate'
+import type { Plate } from '@/types/types'
 
-interface PlateProps {
-  name: string
-  src: string
-}
-
-function Plate({ name, src }: PlateProps): JSX.Element {
-  const [unavailable, setUnavailable] = useState(false)
+export default function Home(): JSX.Element {
+  const [open, setOpen] = useState(false)
+  const [plates, setPlates] = useState<Plate[]>([])
 
   const handleClick = (): void => {
-    setUnavailable(!unavailable)
+    setOpen(!open)
+  }
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault()
+    const form = e.target as HTMLFormElement
+    const formData = new FormData(form)
+
+    const req = await fetch('/api/plate', {
+      method: 'POST',
+      body: formData
+    })
+
+    const res = await req.json()
+    console.log(res)
   }
 
   useEffect(() => {
-    console.log(unavailable)
-  }, [unavailable])
+    ;(async () => {
+      const req = await fetch('api/plate')
+      const res = await req.json()
+      setPlates(res.plates)
+    })().catch((e: Error) => {
+      console.log(e.message)
+    })
+  }, [])
 
-  return (
-    <div
-      className={`border-b-2 border-dark pb-2 last:border-b-0 ${
-        unavailable ? 'grayscale' : ''
-      }`}
-    >
-      <article
-        className={`flex items-center gap-3 rounded-sm p-2 ${
-          unavailable ? 'bg-slate-700' : ''
-        }`}
-        onClick={handleClick}
-      >
-        <div className='relative h-32 w-32 overflow-hidden rounded-sm object-cover'>
-          <Image
-            alt={name}
-            src={src}
-            fill
-            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-          />
-        </div>
-        <span className={`grow ${unavailable ? 'line-through' : ''}`}>
-          {name}
-        </span>
-      </article>
-    </div>
-  )
-}
+  // RESOURCES
+  const typeOrderOptions: Array<Plate['order']> = ['Entrante', 'Segundo']
 
-interface OrderPlateProps {
-  children: React.ReactNode
-  typeOrder: string
-}
-
-function OrderPlate({ children, typeOrder }: OrderPlateProps): JSX.Element {
-  return (
-    <div className='flex flex-col gap-2 bg-superdark p-4'>
-      <h3 className='my-3 rounded-sm bg-dark text-center italic'>
-        {typeOrder}
-      </h3>
-      {children}
-    </div>
-  )
-}
-
-export default function Home(): JSX.Element {
   return (
     <main className='flex min-h-screen justify-center p-8'>
       <div className='flex w-full max-w-xl flex-col gap-5'>
+        <form
+          className='flex flex-col gap-2'
+          onSubmit={handleSubmit}
+          encType='multipart/form-data'
+        >
+          <section className='flex flex-col'>
+            <label>Nombre del plato</label>
+            <input name='name' />
+          </section>
+          <section className='flex flex-col'>
+            <label>Imagen</label>
+            <input name='image' type='file' />
+          </section>
+          <section className='flex flex-col'>
+            <label>Tipo de orden</label>
+            <select className='p-2' name='order'>
+              {typeOrderOptions.map(typeOrder => {
+                return (
+                  <option key={crypto.randomUUID()} value={typeOrder}>
+                    {typeOrder}
+                  </option>
+                )
+              })}
+            </select>
+          </section>
+          <input
+            className='cursor-pointer rounded-sm bg-superdark px-8 py-2'
+            type='submit'
+          />
+        </form>
         <div className='flex items-center justify-center gap-2 rounded-md bg-superdark px-8  py-2 font-bold italic'>
-          <div className='relative h-9 w-9 object-cover'>
+          <div className='relative h-9 w-9 shrink-0 object-cover'>
             <Image
               alt='chef-hat'
               src='/favicon.ico'
@@ -77,22 +86,42 @@ export default function Home(): JSX.Element {
             />
           </div>
           <h1 className='text-center leading-none'>Restaurante SF</h1>
-          <span className='rounded-sm bg-emerald-400 px-3 py-1'>Abierto</span>
+          <span
+            className={`cursor-pointer rounded-sm px-3 py-1 ${
+              open ? 'bg-emerald-400' : 'bg-red-400'
+            }`}
+            onClick={handleClick}
+          >
+            {open ? 'Abierto' : 'Cerrado'}
+          </span>
         </div>
         <section className='flex flex-col gap-[2px] rounded-sm border-2 border-light bg-light'>
           <h2 className='bg-superdark p-2 text-center font-bold'>
             Menú - 2.5$ Completo
           </h2>
           <OrderPlate typeOrder='Entrante - 1.5$ solo, sin segundo'>
-            <Plate name='Sopa de lentejas' src='/sopa-de-lentejas.png' />
-            <Plate name='Sopa de fideos' src='/sopa-fideos.png' />
+            {plates.map(plate => {
+              if (plate.order !== 'Entrante') return false
+              return (
+                <CardPlate
+                  key={crypto.randomUUID()}
+                  name={plate.name}
+                  src={plate.image}
+                />
+              )
+            })}
           </OrderPlate>
           <OrderPlate typeOrder='Segundo - 1.5$ solo, sin entrante'>
-            <Plate name='Pollo al horno' src='/arroz-con-pollo-al-horno.png' />
-            <Plate
-              name='Tortilla de camaron'
-              src='/arroz-con-tortilla-de-camaron.png'
-            />
+            {plates.map(plate => {
+              if (plate.order !== 'Segundo') return false
+              return (
+                <CardPlate
+                  key={crypto.randomUUID()}
+                  name={plate.name}
+                  src={plate.image}
+                />
+              )
+            })}
           </OrderPlate>
         </section>
       </div>
